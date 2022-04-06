ea0b65ac97606408d2cbe03c0b3cdbec40b1c840
VaxPex <97077459+VaxPex@users.noreply.github.com>
2022-04-06 11:29:05 +0300

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -51,7 +51,7 @@ class Main extends PluginBase implements Listener
 		}
 		StringToItemParser::getInstance()->registerBlock("netherite_block", fn() => BlockFactory::getInstance()->get(525, 0));
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		BlockFactory::getInstance()->register(new Block(new BlockIdentifier(525, 0, -270), "Block of Netherite", new BlockBreakInfo(50, BlockToolType::PICKAXE, 5, 6000)));
+		BlockFactory::getInstance()->register(new Block(new BlockIdentifier(525, 0, -270), "Block of Netherite", new BlockBreakInfo(50, BlockToolType::PICKAXE, 5, 1.200)));
 		$creativeItems = json_decode(file_get_contents(Path::join(\pocketmine\BEDROCK_DATA_PATH, "creativeitems.json")), true);
 		foreach ($creativeItems as $data) {
 			$item = Item::jsonDeserialize($data);
@@ -68,57 +68,5 @@ class Main extends PluginBase implements Listener
 			}
 			CreativeInventory::getInstance()->add($item);
 		}
-	}
-
-	//TODO: onChunkLoad to fix it from turning to reversed6(255)
-
-	public function getFullBlock(World $world, int $x, int $y, int $z): int
-	{
-		return $world->getChunk($x >> 4, $z >> 4)->getFullBlock($x & 0x0f, $y, $z & 0x0f);
-	}
-
-	public function sendBlocks(World $world, array $target, array $blocks, int $flags = UpdateBlockPacket::FLAG_NONE, bool $optimizeRebuilds = false): void
-	{
-		$packets = [];
-		if ($optimizeRebuilds) {
-			$chunks = [];
-			foreach ($blocks as $b) {
-				if (!($b->getPosition()->asVector3() instanceof Vector3)) {
-					throw new \TypeError("Expected Vector3 in blocks array, got " . (is_object($b) ? get_class($b) : gettype($b)));
-				}
-				$pk = new UpdateBlockPacket();
-				$first = false;
-				if (!isset($chunks[$index = World::chunkHash($b->x >> 4, $b->z >> 4)])) {
-					$chunks[$index] = true;
-					$first = true;
-				}
-				$pk->blockPosition = new BlockPosition($b->getPosition()->x, $b->getPosition()->y, $b->getPosition()->z);
-				if ($b instanceof Block) {
-					$pk->blockRuntimeId = RuntimeBlockMapping::getInstance()->toRuntimeId($b->getFullId());
-				} else {
-					$fullBlock = $this->getFullBlock($world, $b->getPosition()->x, $b->getPosition()->y, $b->getPosition()->z);
-					$pk->blockRuntimeId = RuntimeBlockMapping::getInstance()->toRuntimeId(BlockFactory::getInstance()->get($fullBlock >> 4, $fullBlock & 0xf)->getFullId());
-				}
-				$pk->flags = $first ? $flags : UpdateBlockPacket::FLAG_NONE;
-				$packets[] = $pk;
-			}
-		} else {
-			foreach ($blocks as $b) {
-				if (!($b->getPosition()->asVector3() instanceof Vector3)) {
-					throw new \TypeError("Expected Vector3 in blocks array, got " . (is_object($b) ? get_class($b) : gettype($b)));
-				}
-				$pk = new UpdateBlockPacket();
-				$pk->blockPosition = new BlockPosition($b->getPosition()->x, $b->getPosition()->y, $b->getPosition()->z);
-				if ($b instanceof Block) {
-					$pk->blockRuntimeId = RuntimeBlockMapping::getInstance()->toRuntimeId($b->getFullId());
-				} else {
-					$fullBlock = $this->getFullBlock($world, $b->getPosition()->x, $b->getPosition()->y, $b->getPosition()->z);
-					$pk->blockRuntimeId = RuntimeBlockMapping::getInstance()->toRuntimeId(BlockFactory::getInstance()->get($fullBlock >> 4, $fullBlock & 0xf)->getFullId());
-				}
-				$pk->flags = $flags;
-				$packets[] = $pk;
-			}
-		}
-		$this->getServer()->broadcastPackets($target, $packets);
 	}
 }
